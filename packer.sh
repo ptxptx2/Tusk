699754a074e4d08219059dad4652836ba5ae1b37
perci <ptieng@gmail.com>
2022-11-07 18:59:10 -0800

    made local changes

--- packer.sh
+++ packer.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# modified to put it in dist directory
+
 TARGET_BROWSER=$1
 [ -z $TARGET_BROWSER ] && echo "Usage: ./packer.sh [chrome|firefox]" && exit 1
 
+TARGET_DIR="dist"
+[ -d $TAGEET_DIR ] || mkdir $TARGET_DIR
+
 cp $TARGET_BROWSER.manifest.json manifest.json
 zip -r $TARGET_BROWSER.tusk.zip build/ assets/icons/exported/ assets/other/ dll/ popup.html options.html manifest.json
-rm -rf ./demo-$TARGET_BROWSER
-mkdir -p demo-$TARGET_BROWSER
-cp $TARGET_BROWSER.tusk.zip ./demo-$TARGET_BROWSER
-cd ./demo-$TARGET_BROWSER
+
+rm -rf ${TARGET_DIR}/demo-$TARGET_BROWSER
+mkdir -p ${TARGET_DIR}/demo-$TARGET_BROWSER
+mv $TARGET_BROWSER.tusk.zip ${TARGET_DIR}/demo-$TARGET_BROWSER
+cd ${TARGET_DIR}/demo-$TARGET_BROWSER
 unzip *
 cd ..
-echo "Files packed to $TARGET_BROWSER.tusk.zip.  Verify the build in ./demo-$TARGET_BROWSER"
+echo "Files packed to ${TARGET_DIR}/$TARGET_BROWSER.tusk.zip.  Verify the build in ${TARGET_DIR}/demo-$TARGET_BROWSER"
